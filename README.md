4c6f4dcac27fb849319c25a1981c75ca8d5d8379
Marcin Hebdzyński <vyzrala@gmail.com>
2020-10-22 18:01:09 +0200

    installable package attempt 3

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # StockPredictorLSTM
-Thesis project
+## Thesis project of 4 masculine men :muscle:
+
+
+Installable package and manual are in *.zip file under 'StockPredictorLSTM/distribution' directory.
+
--- setup.py
+++ setup.py
@@ -4,14 +4,26 @@ with open('README.md', 'r') as ld:
     long_description = ld.read()
 
 setuptools.setup(
-    name="StockPredictorLSTM-Vyzrala",
+    name="StockPredictorLSTM",
     version="0.1.1",
     description="Thesis project",
     long_description=long_description,
-    author="Marcin Hebdzyński",
+    author="Marcin Hebdzynski",
     author_email="hebdzynski.m@gmail.com",
+    license='MIT License',
     url="https://github.com/Vyzrala/StockPredictorLSTM",
-    packages=setuptools.find_packages(include=["StockPredictorLSTM", "StockPredictorLSTM.*"], exclude=["tests"], where="StockPredictorLSTM"),
-    package_dir = {":":"StockPredictorLSTM"},
+    packages=["StockPredictorLSTM"], #setuptools.find_packages(include=["StockPredictorLSTM", "StockPredictorLSTM.*"], exclude=["tests"], where="StockPredictorLSTM"),
     python_requires=">=3.7",
+    install_requires=[
+        'pandas>=1.1.1',
+        'tensorflow>=2.3.0',
+        'tensorflow-estimator>=2.3.0',
+        'numpy>=1.18.5',
+        'seaborn>=0.10.1',
+        'Keras>=2.4.3',
+        'scipy>=1.4.1',
+        'pandas-datareader>=0.9.0',
+        'holidays>=0.10.3'
+    ],
+    package_dir = {":":"StockPredictorLSTM"},
 )
