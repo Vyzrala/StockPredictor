dc729db4b933ba0bc362bffa64c16bfefc287dd3
Marcin Hebdzyński <vyzrala@gmail.com>
2020-10-24 13:19:15 +0200

    new distribution

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as ld:
 
 setuptools.setup(
     name="StockPredictorLSTM",
-    version="0.1.2",
+    version="0.1.3",
     description="Thesis project",
     long_description=long_description,
     author="Marcin Hebdzynski",
