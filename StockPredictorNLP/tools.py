2aca5c51baca3334d524f41072c10b8ec0c69956
Marcin Hebdzyński <vyzrala@gmail.com>
2020-11-15 22:05:04 +0100

    saving modified

--- StockPredictorNLP/tools.py
+++ StockPredictorNLP/tools.py
@@ -8,7 +8,7 @@ from pathlib import Path
 settings = {
     'format': '%(asctime)s | %(levelname)s | %(funcName)s | %(lineno)s | %(message)s',
     'log_file': '/tools.log',
-    'log_folder': os.getcwd() + '/NLP' 
+    'log_folder': os.getcwd(),
 }
 Path(settings['log_folder']).mkdir(parents=True, exist_ok=True)
 logging.basicConfig(
