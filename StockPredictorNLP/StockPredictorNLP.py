06507dfa005d660e07b2756ff32053503a230eaf
Marcin Hebdzyński <vyzrala@gmail.com>
2020-11-17 23:54:37 +0100

    final model training works

--- StockPredictorNLP/StockPredictorNLP.py
+++ StockPredictorNLP/StockPredictorNLP.py
@@ -1,20 +1,20 @@
+from keras.layers import Dense, LSTM, GRU, SimpleRNN
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential
+import matplotlib.pyplot as plt
+from typing import Tuple, Dict
+import time, datetime, os
+import tensorflow as tf
+import seaborn as sb
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sb
-from sklearn.preprocessing import MinMaxScaler
-from keras.layers import Dense, LSTM, GRU, SimpleRNN
-from keras.models import Sequential
-from sklearn.metrics import mean_squared_error
-import tensorflow as tf
-import time, datetime, os
-from typing import Tuple
 
 
 class PredictorNLP:
-    def __init__(self, 
-                 split_ratio: float=0.8, 
-                 epochs_number: int=10) -> None:
+    def __init__(self, split_ratio: float=0.8, 
+                 epochs_number: int=10,
+                 batch_size: int=1) -> None:
         self.layers = [LSTM, GRU, SimpleRNN]
         self.scaler = MinMaxScaler(feature_range=(0, 1))
         self.features = ['Open', 'Close', 'Volume', 'Polarity', 'Subjectivity']
@@ -26,8 +26,9 @@ class PredictorNLP:
         self.best_model_stuff = {}
         self.rmse = 0
         self.last_prediction = 0
+        self.batch_size = batch_size
     
-    def create_model(self, dataset: pd.DataFrame):
+    def create_model(self, dataset: pd.DataFrame) -> None:
         self.raw_dataset = dataset.copy()
         
         # Process dataset
@@ -46,47 +47,60 @@ class PredictorNLP:
         # create models
         models = {}
         for layer in self.layers:
+            layer_name = str(layer).split('.')[-1][:-2]
             tmp_model = self.initialize_model(layer, shape)
             tmp_history = tmp_model.fit(x_train, y_train, 
-                                epochs=self.epochs_number, 
-                                batch_size=1, 
-                                validation_split=.05)
+                                        epochs=self.epochs_number, 
+                                        batch_size=self.batch_size, 
+                                        validation_split=.05)
             tmp_predictions = tmp_model.predict(x_test)
             unscaled_predictions = ((tmp_predictions.flatten() - self.scaler.min_[0]) 
                                     / self.scaler.scale_[0])
             unscaled_y_test = ((y_test.flatten() - self.scaler.min_[0]) 
                                / self.scaler.scale_[0])
             tmp_rmse = np.sqrt(mean_squared_error(unscaled_y_test, unscaled_predictions))
-            print("\n{}: RMSE = {}\n".format(str(layer).split('.')[-1][:-2], tmp_rmse))
-            models[layer] = {'model': tmp_model, 
-                             'history': tmp_history, 
-                             'predictions': unscaled_predictions,
-                             'rmse':tmp_rmse,
-                             'layer':str(layer).split('.')[-1][:-2]}
+            msg = '{}: RMSE = {}'.format(layer_name, tmp_rmse)
+            print('\n\t', msg, '\n')
+            models[layer_name] = {'layer': layer_name,
+                                  'model': tmp_model, 
+                                  'rmse': tmp_rmse,
+                                  'history': tmp_history, 
+                                  'predictions': unscaled_predictions}
 
-        # select best model
+        # Select best model by the lowest RMSE value
         self.models = models
         best_model_stuff = min(models.values(), key=lambda x: x['rmse'])
-        print(best_model_stuff)
         
         # Final training
+        final_dataset = self.scaler.fit_transform(dfa)
+        final_x, final_y = self.get_xy_sets(final_dataset)
+        model = best_model_stuff['model']
+        model.fit(final_x, final_y, 
+                  epochs=self.epochs_number,
+                  batch_size=self.batch_size,
+                  validation_split=.05)
+        model.summary()
+        self.model = model
+        self.rmse = best_model_stuff['rmse']
+        self.best_model_data = best_model_stuff
+        print("\nBest model ({}) has been fully trained.".format(best_model_stuff['layer']))
     
-    def predict(self):
+    def predict(self) -> float:
         pass
     
-    def save_model(self, path: str):
+    def save_model(self, path: str) -> bool:
         pass
     
-    def load_model(self, path: str):
+    def load_model(self, path: str) -> bool:
         pass
 
-    def get_data_from_file(self, path_to_file):
+    def get_data_from_file(self, path_to_file: str) -> pd.DataFrame:
         dataset = None
         if os.path.isfile(path_to_file):
             dataset = pd.read_csv(path_to_file)  
             return dataset          
         else:
-            raise NLPError('\nNo such file.\nPlease check if file at {} exists.'.format(path_to_file))
+            raise NLPError('\nNo such file.\nPlease check if file exists at {}.'.format(path_to_file))
     
     def get_xy_sets(self, dataset: np.array) -> Tuple[np.array, np.array]:
         x, y = [], []
@@ -96,7 +110,7 @@ class PredictorNLP:
         
         return (np.array(x), np.array(y))
     
-    def initialize_model(self, layer, shape, show_summary=False) -> None:
+    def initialize_model(self, layer, shape, show_summary=False):
         tf.keras.backend.clear_session()
         model = Sequential()
         model.add(layer(50, activation='relu', return_sequences=True, input_shape=shape))
@@ -110,6 +124,9 @@ class PredictorNLP:
         if show_summary: 
             model.summary()
         return model
+    
+    def get_models_comparison_graph(self, data: Dict[str, dict]):
+        pass
 
 
 class NLPError(Exception): pass
